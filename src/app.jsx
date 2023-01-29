c3913fab3f615aa6e5d9438818bfdb1c92604780
Aleksandr60rus <113985770+Aleksandr60rus@users.noreply.github.com>
2023-01-29 17:52:07 +0200

    newChanges

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import CountersList from "./components/countersList"
+
+import EpisodesList from "./components/episodesList";
+
+function App() {
+  return (
+    <div className="App">
+      <EpisodesList />
+    </div>
+  );
+}
+
+const App = () => {
+    return <CountersList />
+}
+
+export default App
+
